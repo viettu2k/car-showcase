ae3778dd5cbc1df55d8984b523b562bd3e094272
viettu2k <doanviettu2k@gmail.com>
2023-07-16 10:04:03 +0700

    adding CarCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import Image from 'next/image';
 
-import { CustomFilter, Hero, SearchBar } from '@/components';
+import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
+import { fetchCars } from '@/utils';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  console.log('🚀 ~ file: page.tsx:8 ~ Home ~ allCars:', allCars);
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className='overflow-hidden'>
       <Hero />
@@ -18,6 +24,20 @@ export default function Home() {
             <CustomFilter title='year' />
           </div>
         </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car, index) => (
+                <CarCard key={index} car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,31 @@
-import { MouseEventHandler } from "react";
+import { MouseEventHandler } from 'react';
 
 export interface CustomButtonProps {
-    title: string;
-    containerStyles?: string;
-    handleClick?: MouseEventHandler<HTMLButtonElement>;
-    btnType?: 'button' | 'submit'
+  isDisabled?: boolean;
+  btnType?: 'button' | 'submit';
+  containerStyles?: string;
+  textStyles?: string;
+  title: string;
+  rightIcon?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface SearchManufacturerProps {
-    manufacturer: string;
-    setManufacturer: (manufacturer: string) => void;
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
