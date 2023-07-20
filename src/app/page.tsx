debf21270dd0208c810dd2d912b42cc2e795ecba
viettu2k <doanviettu2k@gmail.com>
2023-07-20 20:51:38 +0700

    adding custom filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,22 +17,18 @@ export default async function Home({ searchParams }: HomeProps) {
   return (
     <main className='overflow-hidden'>
       <Hero />
-
       <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
           <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
           <p>Explore out cars you might like</p>
         </div>
-
         <div className='home__filters'>
           <SearchBar />
-
           <div className='home__filter-container'>
-            {/* <CustomFilter title='fuel' options={fuels} />
-            <CustomFilter title='year' options={yearsOfProduction} /> */}
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
           </div>
         </div>
-
         {!isDataEmpty ? (
           <section>
             <div className='home__cars-wrapper'>
